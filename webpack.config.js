9ca6f61df9e50af48112ebbe16b46e72d216e3f7
Zhuo Chen <zhuo.h.chen@gmail.com>
2017-10-16 17:28:08 -0400

    Reinitialize npm for new repo.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-  entry: './lib/root.js',
+  entry: 'lib/root.js',
   output: {
     path: path.resolve(__dirname, 'lib'),
     filename: 'bundle.js',
